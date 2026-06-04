417d9160fbe65e0b0a16e16a9af11595ec7bc88c
Pyt-git <weicky0804@gmail.com>
2026-06-04 15:51:25 -0400

    Update create.php

--- CRUD_projects/transit_route_notes/create.php
+++ CRUD_projects/transit_route_notes/create.php
@@ -1,1 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Create Route Notes</title>
+  </head>
+  <body>
 
+    <h1>Add new transit route</h1>
+    <form action="store.php" method="POST">
+      <select name="route_id">
+        <option value="1">150 René-Lévesque</option>
+        <option value="2">165 Côte-des-Neiges</option>
+        <option value="3">48 Perras</option>
+        <option value="4">REM A1 Brossard</option>
+      </select>
+    
+    </form>
+  </body>
+</html>

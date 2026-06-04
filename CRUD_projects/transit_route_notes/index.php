0e85730ece71dc667aee193e64b03c12728e63f0
Pyt-git <weicky0804@gmail.com>
2026-06-04 16:11:26 -0400

    Create index.php

---
+++ CRUD_projects/transit_route_notes/index.php
@@ -0,0 +1,50 @@
+SELECT notes.id, notes.note_text, notes.note_time, routes.route_number, routes.route_name
+FROM notes
+JOIN routes ON notes.route_id = routes.id;
+
+<?php
+include "db.php";
+$sql = "SELECT notes.id, notes.note_text, notes.note_time, routes.route_number, routes.route_name
+        FROM notes
+        JOIN routes ON notes.route_id = routes.id";
+
+$result = $conn->query($sql);
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Product Catalog</title>
+  </head>
+  <body>
+    <h1>Product catalog</h1>
+
+    <a href="create.php">Add new product</a>
+    <br>
+    <br>
+
+    <table border="1" cellpadding="10">
+      <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Price</th>
+        <th>Category</th>
+        <th>Quantity</th>
+      </tr>
+
+      <?php while ($row = $result->fetch_assoc()): ?>
+      <tr>
+        <td><?=$row['id']?></td>
+        <td><?=$row['name']?></td>
+        <td><?=$row['price']?></td>
+        <td><?=$row['category']?></td>
+        <td><?=$row['quantity']?></td>
+        <td>
+          <a href="edit.php?id<?=$row['id']">Edit</a> |
+          <a href="delete.php?id<?=$row['id']">Delete</a>
+        </td>
+      </tr>
+      <?php endwhile;?>
+    </table>
+  </body>
+</html>

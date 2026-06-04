63ad3a1f42e9f49c903d8f9e43bf179f22281a9c
Pyt-git <weicky0804@gmail.com>
2026-06-04 16:03:11 -0400

    Update store.php

--- CRUD_projects/transit_route_notes/store.php
+++ CRUD_projects/transit_route_notes/store.php
@@ -2,10 +2,8 @@
 include "db.php"; 
 if ($SERVER["REQUEST_METHOD"] == "POST") {
   $route_id = $_POST['route_id'];
-  $n = $_POST['price'];
-  $category = $_POST['category'];
-  $quantity = $_POST['quantity'];
-  $sql = "INSERT INTO products(id, name, price, category, quantity)VALUES('$name', '$price', '$category', '$quantity')";
+  $note_text = $_POST['note_text'];
+  $sql = "INSERT INTO products(route_id, note_text)VALUES('$route_id', '$note_text')";
   $conn->query($sql);
   header("location: index.php");
   exit;
